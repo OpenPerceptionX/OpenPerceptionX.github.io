c9b49cf12a7a01c52b90ff66e5023f05b6b4b59c
faikit <huijie.wang@hotmail.com>
2023-06-30 00:11:59 +0800

    .

--- datasets/OpenLane-V2/tack_0.js
+++ datasets/OpenLane-V2/tack_0.js
@@ -2,226 +2,227 @@ $(function () {
     // 定义的数据
     var allData_0 = [
         {
-            "Organization": "Mach Drive<br>迈驰智行",
+            "Organization": "MEGVII Technology<br>旷视科技",
             "Country" : "/style/cvpr2023/img/country/CHN.svg",
-            "Rank": "1<img src='/style/cvpr2023/img/rank01.png' style='padding: 0px 0px 0px 5px; width: 20px; user-select: none;'/><img src='/style/cvpr2023/img/blomb1.png' style='padding: 0px 0px 0px 5px; width: 20px; user-select: none;'/>",
-            "team": "MACH <br> <a href=\"/e2ead/AD23Challenge/Track_0_MACH.pdf\">[paper]</a>",
-            "mAP": 83.5,
-            "Ped_Crossing": 86.66,
-            "Divider": 81.54,
-            "Boundary": 82.29
+            "Rank": 1,
+            "date": "2023-06-01",
+            "Team": "MFV <br> <a href=\"/e2ead/AD23Challenge/Track_1_MFV.pdf\">[paper]</a> <a href=\"https://arxiv.org/pdf/2306.09590.pdf\">[arXiv]</a>",
+            "prize" : "/style/cvpr2023/img/rank01.png",
+            "DET_l": 0.3577,
+            "DET_t": 0.7970,
+            "TOP_ll": 0.2252,
+            "TOP_lt": 0.3348,
+            "OLS": 0.5519,
+            "Fscore": 0.2559,
+            "option": "yes"
         },
         {
-            "Organization": "Independent Researcher",
+            "Organization": "QCraft<br>轻舟智航",
+            "Country" : "/style/cvpr2023/img/country/CHN.svg",
+            "Rank": "-",
+            "date": "2023-05-17",
+            "prize" : "",
+            "Link": "",
+            "Team": "qcraft2",
+            "DET_l": 0.4168,
+            "DET_t": 0.6374,
+            "TOP_ll": 0.0657,
+            "TOP_lt": 0.3037,
+            "OLS": 0.4654,
+            "Fscore": 0.4805
+        },
+        {
+            "Organization": "AMD",
             "Country" : "/style/cvpr2023/img/country/USA.svg",
-            "Rank": "2<img src='/style/cvpr2023/img/rank02.png' style='padding: 0px 0px 0px 5px; width: 20px; user-select: none;'/>",
-            "team": "MapNeXt",
-            "mAP": 73.65,
-            "Ped_Crossing": 68.94,
-            "Divider": 76.66,
-            "Boundary": 75.34,
-            "option": 'yes'
+            "Rank": 2,
+            "date": "2023-06-01",
+            "prize" : "/style/cvpr2023/img/rank02.png",
+            "Link": "",
+            "Team": "Victory <a href=\"/e2ead/AD23Challenge/Track_1_Victory.pdf\">[paper]</a>",
+            "DET_l": 0.2184,
+            "DET_t": 0.7245,
+            "TOP_ll": 0.1324,
+            "TOP_lt": 0.2261,
+            "OLS": 0.4456,
+            "Fscore": 0.2530
         },
         {
-            "Organization": "Shanghai Jiao Tong University<br>上海交通大学",
+            "Organization": "QCraft<br>轻舟智航",
             "Country" : "/style/cvpr2023/img/country/CHN.svg",
+            "Rank": "-",
+            "date": "2023-05-23",
+            "prize" : "",
+            "Link": "",
+            "Team": "qcraft-team",
+            "DET_l": 0.2960,
+            "DET_t": 0.6374,
+            "TOP_ll": 0.0463,
+            "TOP_lt": 0.2469,
+            "OLS": 0.4114,
+            "Fscore": 0.3467
+        },
+        {
+            "Organization": "Middle East Technical University",
+            "Country" : "/style/cvpr2023/img/country/TUR.svg",
             "Rank": 3,
-            "team": "SJTUCR  <br> <a href=\"/e2ead/AD23Challenge/Track_0_SJTUCR.pdf\">[paper]</a>",
-            "mAP": 73.39,
-            "Ped_Crossing": 70.37,
-            "Divider": 75.08,
-            "Boundary": 74.73
+            "date": "2023-06-01",
+            "prize" : "/style/cvpr2023/img/blomb1.png",
+            "Link": "<a href=\"/e2ead/AD23Challenge/Track_1_PlatypusWhisperers.pdf\">[paper]</a>",
+            "Team": "PlatypusWhisperers <br> <a href=\"/e2ead/AD23Challenge/Track_1_PlatypusWhisperers.pdf\">[paper]</a> <a href=\"https://arxiv.org/pdf/2306.05419.pdf\">[arXiv]</a>",
+            "DET_l": 0.2209,
+            "DET_t": 0.7061,
+            "TOP_ll": 0.0602,
+            "TOP_lt": 0.1570,
+            "OLS": 0.3922,
+            "Fscore": 0.4592
         },
         {
-            "Organization": "Lotus NYO",
-            "Country" : "/style/cvpr2023/img/country/UK.svg",
+            "Organization": "MeiTuan<br>美团",
+            "Country" : "/style/cvpr2023/img/country/CHN.svg",
             "Rank": 4,
-            "team": "LTS (MapLTS2) <br> <a href=\"/e2ead/AD23Challenge/Track_0_LTS.pdf\">[paper]</a>",
-            "mAP": 72.56,
-            "Ped_Crossing": 72.67,
-            "Divider": 73.2,
-            "Boundary": 71.8
+            "date": "2023-06-01",
+            "prize" : "",
+            "Link": "<a href=\"/e2ead/AD23Challenge/Track_1_gavin.pdf\">[paper]</a>",
+            "Team": "gavin <br> <a href=\"/e2ead/AD23Challenge/Track_1_gavin.pdf\">[paper]</a>",
+            "DET_l": 0.1790,
+            "DET_t": 0.7028,
+            "TOP_ll": 0.0401,
+            "TOP_lt": 0.2112,
+            "OLS": 0.3854,
+            "Fscore": 0.2478
         },
         {
-            "Organization": "University of Science and Technology of China<br>中国科学技术大学",
+            "Organization": "Beihang University<br>北京航空航天大学",
             "Country" : "/style/cvpr2023/img/country/CHN.svg",
             "Rank": 5,
-            "team": "ustc_vgg",
-            "mAP": 71.02,
-            "Ped_Crossing": 69.05,
-            "Divider": 73.24,
-            "Boundary": 70.76
+            "date": "2023-06-01",
+            "prize" : "",
+            "Link": "",
+            "Team": "qwertyczx (e110_r)",
+            "DET_l": 0.2643,
+            "DET_t": 0.6607,
+            "TOP_ll": 0.0317,
+            "TOP_lt": 0.1918,
+            "OLS": 0.3853,
+            "Fscore": 0.2817
         },
         {
-            "Organization": "Xi'an Jiaotong University<br>西安交通大学",
-            "Country" : "/style/cvpr2023/img/country/CHN.svg",
+            "Organization": "Turing Inc.",
+            "Country" : "/style/cvpr2023/img/country/JPN.svg",
             "Rank": 6,
-            "team": "XJTU-IAIR",
-            "mAP": 70.43,
-            "Ped_Crossing": 64.69,
-            "Divider": 74.52,
-            "Boundary": 72.07
+            "date": "2023-06-01",
+            "prize" : "",
+            "Link": "",
+            "Team": "turing-machine",
+            "DET_l": 0.1335,
+            "DET_t": 0.7864,
+            "TOP_ll": 0.0148,
+            "TOP_lt": 0.1266,
+            "OLS": 0.3493,
+            "Fscore": 0.2494
         },
         {
             "Organization": "-",
             "Country" : "/style/cvpr2023/img/country/blank.png",
             "Rank": 7,
-            "team": "MapVision",
-            "mAP": 69.71,
-            "Ped_Crossing": 65.58,
-            "Divider": 73.48,
-            "Boundary": 70.06
+            "date": "2023-06-01",
+            "prize" : "",
+            "Link": "",
+            "Team": "Haoqing",
+            "DET_l": 0.0863,
+            "DET_t": 0.7187,
+            "TOP_ll": 0.0369,
+            "TOP_lt": 0.1516,
+            "OLS": 0.3466,
+            "Fscore": 0.4019
         },
         {
             "Organization": "-",
             "Country" : "/style/cvpr2023/img/country/blank.png",
             "Rank": 8,
-            "team": "Qml",
-            "mAP": 68.28,
-            "Ped_Crossing": 63.9,
-            "Divider": 71.73,
-            "Boundary": 69.21
+            "date": "2023-05-29",
+            "prize" : "",
+            "Link": "",
+            "Team": "TopoNet-Anonymous",
+            "DET_l": 0.1950,
+            "DET_t": 0.5842,
+            "TOP_ll": 0.0227,
+            "TOP_lt": 0.1591,
+            "OLS": 0.3322,
+            "Fscore": 0.2757
+        },
+        {        
+            "Organization": "-",
+            "Country" : "/style/cvpr2023/img/country/blank.png",
+            "Rank": 9,
+            "date": "2023-06-01",
+            "prize" : "",
+            "Link": "",
+            "Team": "BVLab",
+            "DET_l": 0.1235,
+            "DET_t": 0.6416,
+            "TOP_ll": 0.0253,
+            "TOP_lt": 0.1558,
+            "OLS": 0.3297,
+            "Fscore": 0.2700
         },
         {
             "Organization": "-",
             "Country" : "/style/cvpr2023/img/country/blank.png",
-            "Rank": 9,
-            "team": "SAITAD",
-            "mAP": 67.34,
-            "Ped_Crossing": 68.56,
-            "Divider": 64.02,
-            "Boundary": 69.45
-        },
-        {
-            "Organization": "GACRD<br>广汽研究院",
-            "Country" : "/style/cvpr2023/img/country/CHN.svg",
             "Rank": 10,
-            "team": "MapSeg  <br> <a href=\"/e2ead/AD23Challenge/Track_0_MapSeg.pdf\">[paper]</a> <a href=\"https://github.com/FlyEgle/CVPR_hdmap\">[code]</a>",
-            "mAP": 67.1,
-            "Ped_Crossing": 63.52,
-            "Divider": 70.33,
-            "Boundary": 67.46
+            "date": "2023-06-01",
+            "prize" : "",
+            "Link": "",
+            "Team": "HDI_EasternExpo",
+            "DET_l": 0.1389,
+            "DET_t": 0.6367,
+            "TOP_ll": 0.0130,
+            "TOP_lt": 0.1546,
+            "OLS": 0.3207,
+            "Fscore": 0.1998
         },
         {
             "Organization": "-",
             "Country" : "/style/cvpr2023/img/country/blank.png",
             "Rank": 11,
-            "team": "IgniteVision",
-            "mAP": 66.41,
-            "Ped_Crossing": 62.04,
-            "Divider": 70.43,
-            "Boundary": 66.76
+            "date": "2023-05-28",
+            "prize" : "",
+            "Link": "",
+            "Team": "ArterySentinel",
+            "DET_l": 0.1182,
+            "DET_t": 0.4459,
+            "TOP_ll": 0.0008,
+            "TOP_lt": 0.0286,
+            "OLS": 0.1904,
+            "Fscore": 0.2175,
+            "option": "yes"
         },
         {
-            "Organization": "-",
-            "Country" : "/style/cvpr2023/img/country/blank.png",
-            "Rank": 12,
-            "team": "LOMG (LOMG)",
-            "mAP": 64.53,
-            "Ped_Crossing": 62.97,
-            "Divider": 66.66,
-            "Boundary": 63.96
-        },
-        {
-            "Organization": "-",
-            "Country" : "/style/cvpr2023/img/country/blank.png",
-            "Rank": 13,
-            "team": "MapKP",
-            "mAP": 64.11,
-            "Ped_Crossing": 59.7,
-            "Divider": 67.62,
-            "Boundary": 65.01
-        },
-        {
-            "Organization": "-",
-            "Country" : "/style/cvpr2023/img/country/blank.png",
-            "Rank": 14,
-            "team": "MapLCF_HuaweiNoah",
-            "mAP": 62.79,
-            "Ped_Crossing": 59.41,
-            "Divider": 67.2,
-            "Boundary": 61.77
-        },
-        {
-            "Organization": "-",
-            "Country" : "/style/cvpr2023/img/country/blank.png",
-            "Rank": 15,
-            "team": "Victory",
-            "mAP": 60.66,
-            "Ped_Crossing": 52.85,
-            "Divider": 66.13,
-            "Boundary": 62.99
-        },
-        {
-            "Organization": "-",
-            "Country" : "/style/cvpr2023/img/country/blank.png",
-            "Rank": 16,
-            "team": "DNTCAD",
-            "mAP": 59.84,
-            "Ped_Crossing": 53.89,
-            "Divider": 64.24,
-            "Boundary": 61.39
-        },
-        {
-            "Organization": "-",
-            "Country" : "/style/cvpr2023/img/country/blank.png",
-            "Rank": 17,
-            "team": "litemap (Litemap)",
-            "mAP": 58.43,
-            "Ped_Crossing": 52.07,
-            "Divider": 62.34,
-            "Boundary": 60.89
-        },
-        {
-            "Organization": "-",
-            "Country" : "/style/cvpr2023/img/country/blank.png",
-            "Rank": 18,
-            "team": "Bell",
-            "mAP": 56.94,
-            "Ped_Crossing": 51.9,
-            "Divider": 61.53,
-            "Boundary": 57.38
-        },
-        {
-            "Organization": "HDI<br>武汉中海庭",
+            "Organization": "Tsinghua University<br>清华大学",
             "Country" : "/style/cvpr2023/img/country/CHN.svg",
-            "Rank": 19,
-            "team": "HDI_Striker (tiny)",
-            "mAP": 52.77,
-            "Ped_Crossing": 48.15,
-            "Divider": 61.36,
-            "Boundary": 48.8
-        },
-        {
-            "Organization": "-",
-            "Country" : "/style/cvpr2023/img/country/blank.png",
-            "Rank": 20,
-            "team": "Ctyun-AI (BaseLine)",
-            "mAP": 42.65,
-            "Ped_Crossing": 36.24,
-            "Divider": 50.95,
-            "Boundary": 40.77
-        },
-        {
-            "Organization": "-",
-            "Country" : "/style/cvpr2023/img/country/blank.png",
-            "Rank": 21,
-            "team": "Host_75980_Team (VectorMapNet-Baseline)",
-            "mAP": 42.11,
-            "Ped_Crossing": 35.95,
-            "Divider": 50.11,
-            "Boundary": 40.26
+            "Rank": 12,
+            "date": "2023-04-03",
+            "prize" : "",
+            "Link": "",
+            "Team": "WeakChicken",
+            "DET_l": 0.0000,
+            "DET_t": 0.6772,
+            "TOP_ll": 0.0000,
+            "TOP_lt": 0.0000,
+            "OLS": 0.1695,
+            "Fscore": 0.0000
         }
     ]
     // 每页显示数据量
     var page_0 = 10
-    // 总条目数
-    var total_0 = allData_0.length
-    // 总页数
-    var pages_0 = Math.ceil(total_0 / page_0)
     // 当前页
     var curPage_0 = 1
-    renderList(allData_0)
+    var selected_data_0 = allData_0
+    renderList(selected_data_0)
+    // 总条目数
+    var total_0 = selected_data_0.length
+    // 总页数
+    var pages_0 = Math.ceil(total_0 / page_0)
     // 渲染数据列表
     function num(i) {
         var n = i.toFixed(2);
@@ -243,21 +244,23 @@ $(function () {
     }
     function renderList(data) {
         // 总页数
+        total_0 = selected_data_0.length
         pages_0 = Math.ceil(total_0 / page_0)
         $(".table_list_0 tbody tr:not(:last-child)").remove()
         data = data.slice((curPage_0 - 1) * page_0, page_0 * curPage_0)
         // 渲染列表数据
         data.forEach((item, index) => {
             var htmlStr = `<tr class="item">
-            <td style="text-align: left;">&nbsp;&nbsp;&nbsp;${item.Rank}</td>
+        <td>${item.date}</td>
+        <td>${item.Team}</td>
         <td>${filter(item.Country)}</td>
         <td>${item.Organization}</td>
-        <td><b>${num(item.mAP)}</b></td>
-        <td>${item.team}</td>
+        <td><b>${num(item.OLS)}</b></td>
         <td>${filter_undefined(item.option)}</td>
-        <td>${num(item.Ped_Crossing)}</td>
-        <td>${num(item.Divider)}</td>
-        <td>${num(item.Boundary)}</td>
+        <td>${num(item.DET_l)}</td>
+        <td>${num(item.DET_t)}</td>
+        <td>${num(item.TOP_ll)}</td>
+        <td>${num(item.TOP_lt)}</td>
       </tr>
 
       `
@@ -270,22 +273,22 @@ $(function () {
     }
     // 根据name进行排序
     $(".table_list_0 tr th:nth-child(1)").click(function () {
-        mysort($(this), 'Rank', allData_0)
+        mysort($(this), 'date', selected_data_0)
     })
-    // $(".table_list_0 tr th:nth-child(2)").click(function () {
-    //     mysort($(this), 'team', allData_0)
-    // })
     $(".table_list_0 tr th:nth-child(5)").click(function () {
-        mysort($(this), 'mAP', allData_0)
-    })
-    $(".table_list_0 tr th:nth-child(6)").click(function () {
-        mysort($(this), 'Ped_Crossing', allData_0)
+        mysort($(this), 'OLS', selected_data_0)
     })
     $(".table_list_0 tr th:nth-child(7)").click(function () {
-        mysort($(this), 'Divider', allData_0)
+        mysort($(this), 'DET_l', selected_data_0)
     })
     $(".table_list_0 tr th:nth-child(8)").click(function () {
-        mysort($(this), 'Boundary', allData_0)
+        mysort($(this), 'DET_t', selected_data_0)
+    })
+    $(".table_list_0 tr th:nth-child(9)").click(function () {
+        mysort($(this), 'TOP_ll', selected_data_0)
+    })
+    $(".table_list_0 tr th:nth-child(10)").click(function () {
+        mysort($(this), 'TOP_lt', selected_data_0)
     })
     // 排序方法
     function mysort(obj, field, data) {
@@ -310,29 +313,29 @@ $(function () {
     // 选择每页显示的数据量
     $("#pageOption_0").change(function () {
         page_0 = $(this).val()
-        renderList(allData_0)
+        renderList(selected_data_0)
     })
     // 上一页
     $("#prev_0").click(function () {
         curPage_0--
         if (curPage_0 <= 0) {
             curPage_0 = 1;
-            $(this).prop('disabled', true)
+            // $(this).prop('disabled', true)
         }
-        $("#next_0").prop('disabled', false)
+        // $("#next_0").prop('disabled', false)
         $("#curPage_0").val(curPage_0)
-        renderList(allData_0)
+        renderList(selected_data_0)
     })
     // 下一页
     $("#next_0").click(function () {
         curPage_0++
         if (curPage_0 >= pages_0) {
             curPage_0 = pages_0;
-            $(this).prop('disabled', true)
+            // $(this).prop('disabled', true)
         }
-        $("#prev_0").prop("disabled", false)
+        // $("#prev_0").prop("disabled", false)
         $("#curPage_0").val(curPage_0)
-        renderList(allData_0)
+        renderList(selected_data_0)
     })
     // 手动输入页码
     $("#curpage_0").change(function () {
@@ -340,18 +343,18 @@ $(function () {
         console.log(inpVal);
         if (inpVal <= 0) {
             curPage_0 = 1
-            $("#prev_0").prop("disabled", true);
-            $("#next_0").prop("disabled", false);
+            // $("#prev_0").prop("disabled", true);
+            // $("#next_0").prop("disabled", false);
         } else if (inpVal > pages_0) {
             curPage_0 = pages_0
-            $("#next_0").prop("disabled", true);
-            $("#prev_0").prop("disabled", false);
+            // $("#next_0").prop("disabled", true);
+            // $("#prev_0").prop("disabled", false);
         } else {
-            $("#next_0").prop("disabled", false);
-            $("#prev_0").prop("disabled", false);
+            // $("#next_0").prop("disabled", false);
+            // $("#prev_0").prop("disabled", false);
             curPage_0 = inpVal
         }
-        renderList(allData_0)
+        renderList(selected_data_0)
     })
     // 筛选
     $("#opt1").change(function () {
@@ -366,12 +369,15 @@ $(function () {
             }
         })
         if (selVal == 'na') {
-            console.log(res1);
-            renderList(res1.slice((curPage_0 - 1) * page_0, page_0 * curPage_0))
+            selected_data_0 = res1;
+            renderList(selected_data_0.slice((curPage_0 - 1) * page_0, page_0 * curPage_0))
         } else if (selVal == 'yes') {
-            renderList(res2.slice((curPage_0 - 1) * page_0, page_0 * curPage_0))
+            selected_data_0 = res2;
+            renderList(selected_data_0.slice((curPage_0 - 1) * page_0, page_0 * curPage_0))
         } else {
-            renderList(allData_0.slice((curPage_0 - 1) * page_0, page_0 * curPage_0))
+            selected_data_0 = allData_0;
+            renderList(selected_data_0.slice((curPage_0 - 1) * page_0, page_0 * curPage_0))
         }
     })
+    $(".table_list_0 tr th:nth-child(5)").click()
 })
