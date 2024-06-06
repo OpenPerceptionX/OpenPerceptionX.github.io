a5f12d459fdf5e4b057b26d88e806e8dc3c95c6e
faikit <huijie.wang@hotmail.com>
2024-06-07 00:49:25 +0800

    .

---
+++ challenge2024/track60.js
@@ -0,0 +1,372 @@
+const track60data = [
+    {
+        team: "CPS",
+        primary: "0.7799",
+        metric1: "0.7861",
+        metric2: "64.8120",
+        country: "CN",
+        institution: "East China Normal University<br>华东师范大学",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "ADLM",
+        primary: "0.7759",
+        metric1: "0.7597",
+        metric2: "64.7224",
+        country: "CN",
+        institution: "Chongqing Changan Automobile Co. Ltd / Chongqing University of Posts and Telecommunications<br>长安汽车 / 重庆邮电大学",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "momo",
+        primary: "0.7668",
+        metric1: "0.7602",
+        metric2: "62.6748",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "Zoeeeeee",
+        primary: "0.7509",
+        metric1: "0.7511",
+        metric2: "65.9302",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "IEIT-AD",
+        primary: "0.7420",
+        metric1: "0.7170",
+        metric2: "61.2355",
+        country: "CN",
+        institution: "Tsinghua University<br>清华大学",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "newbie",
+        primary: "0.7183",
+        metric1: "0.7013",
+        metric2: "61.3644",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "yinsen",
+        primary: "0.7168",
+        metric1: "0.6831",
+        metric2: "61.4857",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "Youngluc",
+        primary: "0.7165",
+        metric1: "0.6831",
+        metric2: "61.3000",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "aquila",
+        primary: "0.7160",
+        metric1: "0.6919",
+        metric2: "61.4443",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "Little5Per",
+        primary: "0.7095",
+        metric1: "0.7294",
+        metric2: "64.6506",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "Course Assignment",
+        primary: "0.7091",
+        metric1: "0.7358",
+        metric2: "64.6090",
+        country: "CN",
+        // institution: "Institute of Software Chinese Academy of Sciences, Nanjing University of Science and Technology, Central South University<br>中国科学院软件研究所、南京理工大学、中南大学",
+        institution: "Institute of Software, Chinese Academy of Sciences<br>中国科学院软件研究所",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "flappy_bird",
+        primary: "0.7053",
+        metric1: "0.6919",
+        metric2: "58.6762",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "Automan",
+        primary: "0.7045",
+        metric1: "0.6919",
+        metric2: "58.4867",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "llavaux",
+        primary: "0.6778",
+        metric1: "0.6793",
+        metric2: "56.4965",
+        country: "CH",
+        institution: "Swiss Federal Institute of Technology in Lausanne",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "lol",
+        primary: "0.6744",
+        metric1: "0.7558",
+        metric2: "62.2205",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "nullptr",
+        primary: "0.6445",
+        metric1: "0.7609",
+        metric2: "62.3453",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "UTT",
+        primary: "0.6189",
+        metric1: "0.7363",
+        metric2: "61.5182",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "luciusy",
+        primary: "0.6187",
+        metric1: "0.6071",
+        metric2: "57.8616",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "C2AD_DriveLM",
+        primary: "0.6567",
+        metric1: "0.6586",
+        metric2: "67.3572",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: true,
+        links: ""
+    },
+    {
+        team: "dudulu66666",
+        primary: "0.6541",
+        metric1: "0.7567",
+        metric2: "59.0882",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "1234asdfadw",
+        primary: "0.6535",
+        metric1: "0.7567",
+        metric2: "59.0377",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "SUSI_Drive",
+        primary: "0.6533",
+        metric1: "0.7567",
+        metric2: "58.9765",
+        country: "CN",
+        // institution: "Tianjin University, Chinese Academy of Sciences' Institute of Automation, University of Cambridge<br>天津大学、中国科学院自动化研究所、剑桥大学",
+        institution: "Tianjin University<br>天津大学",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "mismayil",
+        primary: "0.5947",
+        metric1: "0.6962",
+        metric2: "60.0369",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+    {
+        team: "exiawsh",
+        primary: "0.5884",
+        metric1: "0.7746",
+        metric2: "59.8850",
+        country: "-",
+        institution: "-",
+        award: "blank",
+        disqualified: false,
+        links: ""
+    },
+];
+
+
+let track60sortDirection = "asc";
+track60data.sort((a, b) => b.primary - a.primary);
+index = 0
+track60data.forEach((item, xxx) => {
+    if (item.disqualified) {
+        item.rank = "*";
+    } else {
+        item.rank = index + 1;
+        index = index + 1;
+    }
+});
+
+
+
+let track60currentPage = 1;
+let track60pageSize = 10;
+let track60sortColumn = null;
+
+
+
+function track60render() {
+    const track60tableBody = document.getElementById("track60table");
+    track60tableBody.innerHTML = "";
+    track60data.slice((track60currentPage - 1) * track60pageSize, track60currentPage * track60pageSize).forEach((item, index) => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>${item.rank} <img src="/assets/icon/${item.award}.png" class="inlineimg"/></td>
+            <td><img src="/assets/flags/${item.country}.svg" class="inlineimg"/> ${item.country}</td>
+            <td>${item.institution}</td>
+            <td><b>${item.primary}</b></td>
+            <td>${item.team}</td>
+            <td>${item.links}</td>
+            <td>${item.metric1}</td>
+            <td>${item.metric2}</td>
+        `;
+        track60tableBody.appendChild(row);
+    });
+    document.getElementById("track60pages").innerHTML = `<b>${track60currentPage} / ${Math.ceil(track60data.length / track60pageSize)}</b>`;
+
+    if (window.innerWidth < 1024) {
+        document.getElementById("track60tablefoot").style.width = document.getElementById("track60table").offsetWidth;
+    }
+}
+
+
+
+function track60sort(columnIndex, columnKey) {
+    if (track60sortColumn === columnKey) {
+        track60sortDirection = track60sortDirection === "asc" ? "desc" : "asc";
+    } else {
+        track60sortColumn = columnKey;
+        track60sortDirection = "asc";
+    }
+    track60updateSortIcons(columnIndex);
+    track60data.sort((a, b) => {
+        if (track60sortDirection === "asc") {
+            return a[columnKey] - b[columnKey];
+        } else {
+            return b[columnKey] - a[columnKey];
+        }
+    });
+    track60currentPage = 1;
+    track60render();
+}
+
+
+
+function track60updateSortIcons(columnIndex) {
+    const sortIcons = document.querySelectorAll(".track60button");
+    sortIcons.forEach((icon, index) => {
+        if (index === columnIndex) {
+            if (track60sortDirection === "asc") {
+                icon.innerHTML = icon.innerHTML.replace("&nbsp;&nbsp;&nbsp;", "▲");
+                icon.innerHTML = icon.innerHTML.replace("▼", "▲");
+            } else {
+                icon.innerHTML = icon.innerHTML.replace("&nbsp;&nbsp;&nbsp;", "▼");
+                icon.innerHTML = icon.innerHTML.replace("▲", "▼");
+            }
+        } else {
+            icon.innerHTML = icon.innerHTML.replace("▼", "&nbsp;&nbsp;&nbsp;");
+            icon.innerHTML = icon.innerHTML.replace("▲", "&nbsp;&nbsp;&nbsp;");
+        }
+    });
+}
+
+
+
+function track60previous() {
+    if (track60currentPage > 1) {
+        track60currentPage--;
+        track60render();
+    }
+}
+
+
+
+function track60next() {
+    if (track60currentPage < Math.ceil(track60data.length / track60pageSize)) {
+        track60currentPage++;
+        track60render();
+    }
+}
