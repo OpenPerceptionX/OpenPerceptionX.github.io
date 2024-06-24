a6b0672764249256f80b1ce272d97998f269fb12
faikit <huijie.wang@hotmail.com>
2024-06-24 10:37:48 +0800

    .

--- challenge2024/track1.js
+++ challenge2024/track1.js
@@ -16,7 +16,7 @@ const track1data = [
         institution: "ZERON<br>零一汽车",
         award: "rank02",
         disqualified: false,
-        links: "<a href='https://opendrivelab.github.io/Challenge%202024/e2e_ZERON.pdf' target='_blank'><img class='inlineimg' src='/assets/icon/article.png'/>&nbsp;</a><a href='https://drive.google.com/file/d/1y0GTu0wFR4FMLHqnEpJWbAGl27Mn74uQ/view?usp=sharing' target='_blank'><img class='inlineimg' src='/assets/icon/poster.png'/></a>"
+        links: "<a href='https://opendrivelab.github.io/Challenge%202024/e2e_ZERON.pdf' target='_blank'><img class='inlineimg' src='/assets/icon/article.png'/>&nbsp;</a><a href='https://opendrivelab.github.io/Challenge%202024/others/e2e_ZERON.pdf' target='_blank'><img class='inlineimg' src='/assets/icon/poster.png'/></a>"
     },
     {
         team: "abc",
