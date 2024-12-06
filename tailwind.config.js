556e9c34ed16511c2fc05f51f1a235601904e98c
faikit <huijie.wang@hotmail.com>
2024-12-06 11:31:43 +0800

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,11 @@ module.exports = {
   theme: {
       extend: {
           colors: {
-              "o-blue": "#00a0e8",
-              "o-light-blue": "53c2f0",
-              "o-dark-blue": "0c318f",
+                "o-blue": "#00a0e8",
+                "o-light-blue": "#53c2f0",
+                "o-dark-blue": "#0c318f",
+                "o-white": "#e9e9e9",
+                "o-black": "#212121",
           }
       },
   },
