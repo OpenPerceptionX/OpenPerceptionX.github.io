bc20d30c7bdc282683f61e9a9d3aed4bced695f4
faikit <huijie.wang@hotmail.com>
2024-12-08 00:22:40 +0800

    tailwind: font & screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,27 @@
 module.exports = {
     content: ["./**/*.{html,js}"],
     theme: {
+        screens: {
+            'tablet': '640px',
+            // => @media (min-width: 640px) { ... }
+    
+            'laptop': '1024px',
+            // => @media (min-width: 1024px) { ... }
+    
+            'desktop': '1280px',
+            // => @media (min-width: 1280px) { ... }
+        },
         extend: {
             colors: {
-                  "o-blue": "#00a0e8",
-                  "o-light-blue": "#53c2f0",
-                  "o-dark-blue": "#0c318f",
-                  "o-white": "#e9e9e9",
-                  "o-black": "#212121",
-            }
+                "o-blue": "#00a0e8",
+                "o-light-blue": "#53c2f0",
+                "o-dark-blue": "#0c318f",
+                "o-white": "#e9e9e9",
+                "o-black": "#212121",
+            },
+            fontFamily: {
+                sans: ['InterVariable', ...defaultTheme.fontFamily.sans],
+            },
         },
     },
     plugins: [
