1c098fb4945254daa61c42b40cb019a3e6d7e60d
faikit <huijie.wang@hotmail.com>
2024-12-13 19:17:25 +0800

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
             'tablet': '640px',
             // => @media (min-width: 640px) { ... }
     
-            'laptop': '1280px',
+            'laptop': '1536px',
             // => @media (min-width: 1024px) { ... }
     
             // 'desktop': '1280px',
