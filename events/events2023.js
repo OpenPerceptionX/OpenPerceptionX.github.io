3cc157ca299e979aab1baffff31d7198a8ccb848
faikit <huijie.wang@hotmail.com>
2024-12-25 17:13:08 +0800

    .

--- events/events2023.js
+++ events/events2023.js
@@ -31,7 +31,7 @@ const events2023data = [
         subheading: "Scene Representations for Autonomous Driving",
         link: "/sr4ad/iclr23.html",
         linkoption: '',
-        background: "ui2023/cvpr2023/iclr2023/banner.jpg",
+        background: "/ui2023/cvpr2023/iclr2023/banner.jpg",
         backgroundoption: "bg-center",
         date: "May 05",
     },
@@ -39,7 +39,7 @@ const events2023data = [
         heading: "GAIDC 2023",
         subheading: "数联新出行 开源新发展",
         link: "https://mp.weixin.qq.com/s/hr4WxKO-9ZOJ-zea98-E3A",
-        linkoption: '',
+        linkoption: 'target="_blank"',
         background: "/assets/event/gaidc2023.jpg",
         backgroundoption: "bg-center",
         date: "February 26",
@@ -55,20 +55,16 @@ function events2023render() {
         var innerHTML = `
             <a ${item.linkoption} href="${item.link}" class="hover:opacity-70">
                 <div class="w-full h-72 bg-[url('${item.background}')] ${item.backgroundoption} bg-cover rounded-t-3xl"></div>
-                <div class="felx flex-col p-10 space-y-6 justify-between h-full">
-                    <div class="space-y-6 flex-1">
-                        <h3>
-                            ${item.heading}
-                        </h3>
-                        <h4>
-                            ${item.subheading}
-                        </h4>
-                    </div>
-                    <div class="flex-1 flex flex-col justify-end">
-                        <h4 class="flex-1 text-o-blue text-right pr-3 border-r-4 border-o-blue">
-                            ${item.date}
-                        </h4>
-                    </div>
+                <div class="felx flex-col space-y-6 p-10">
+                    <h3>
+                        ${item.heading}
+                    </h3>
+                    <h4>
+                        ${item.subheading}
+                    </h4>
+                    <h4 class="text-o-blue text-right pr-3 border-r-4 border-o-blue">
+                        ${item.date}
+                    </h4>
                 </div>
             </a>
         `;
