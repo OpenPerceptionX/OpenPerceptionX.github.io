3480d77681c4f327b29e5369a684e11c298792c7
faikit <huijie.wang@hotmail.com>
2024-12-25 13:10:10 +0800

    .

--- team/split1.js
+++ team/split1.js
@@ -1,34 +1,48 @@
 const split1data = [
     {
-        team: "USTC_IAT_United",
-        primary: "0.6615",
-        metric1: "0.5448",
-        metric2: "0.7878",
-        country: "CN",
-        institution: "University of Science and Technology of China<br>中国科学技术大学",
-        award: "rank01",
-        disqualified: false,
-        links: "<a href='https://opendrivelab.github.io/Challenge%202024/predictive_USTC_IAT_United.pdf' target='_blank'><img class='inlineimg' src='/assets/icon/article.png'/></a>"
+        name: "Hongyang Li",
+        link: "https://lihongyang.info",
+        image: "/assets/person/hongyang_li.jpg",
+        note: "Autonomous Driving & Embodied AI",
+        icon: [
+            {
+                type: "home",
+                link: "https://lihongyang.info/"
+            },
+            {
+                type: "home",
+                link: "https://lihongyang.info/"
+            },
+        ],
     },
 ];
 
 
 
 function split1render() {
-    const split1tableBody = document.getElementById("split1table");
-    split1tableBody.innerHTML = "";
-    split1data.forEach((item, index) => {
-        const row = document.createElement("tr");
-        row.innerHTML = `
-            <td>${item.rank} <img src="/assets/icon/${item.award}.png" class="inlineflag"/></td>
-            <td><img src="/assets/flags/${item.country}.svg" class="inlineflag"/> ${item.country}</td>
-            <td>${item.institution}</td>
-            <td><b>${item.primary}</b></td>
-            <td>${item.team}</td>
-            <td>${item.links}</td>
-            <td>${item.metric1}</td>
-            <td>${item.metric2}</td>
+    const split1body = document.getElementById("split1");
+    split1body.innerHTML = "";
+    split1data.forEach((item, _) => {
+        const person = document.createElement("div");
+        person.className = "flex flex-col gap-3 pb-3 border-b-2 border-o-white w-40 tablet:w-48";
+        person.innerHTML = `
+            <a href="${item.link}" target="_blank" class="hover:opacity-70">
+                <img src="${item.image}" class="size-40 tablet:size-48 rounded-3xl"/>
+            </a>
+            <h4>${item.name}</h4>
+            <span>${item.note}</span>
+            <div class="flex flex-row gap-3 flex-wrap">
         `;
-        split1tableBody.appendChild(row);
+        item.icon.forEach((i, _) => {
+            person.innerHTML += `                
+            <a href="${i.link}" target="_blank"> 
+                <img src="/assets/icon/${i.type}.png" class="size-6 hover:opacity-70"/> 
+            </a>
+        `;
+        });
+        person.innerHTML += `
+            </div>
+        `,
+        split1body.appendChild(person);
     });
 }
