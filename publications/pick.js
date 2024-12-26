7884064000a4c9aa9a49ff6d7f0aefe0e3afec14
faikit <huijie.wang@hotmail.com>
2024-12-26 16:17:36 +0800

    .

--- publications/pick.js
+++ publications/pick.js
@@ -183,7 +183,7 @@ function pickrender() {
     pick.forEach((item, _) => {
         var innerHTML = `
             <a href="${item.link}" target="_blank" class="hover:opacity-70 flex flex-row laptop:flex-col justify-center">
-                <img loading="lazy" src="${item.image}" class="w-full laptop:w-96"/>
+                <img loading="lazy" src="${item.image}" class="w-full tablet:w-5/6 laptop:w-96"/>
             </a>
             <div class="flex flex-col justify-center flex-1">
                 <h3>
