68d67cede62ea52505f12eace2d4158807ecdd90
faikit <huijie.wang@hotmail.com>
2024-12-26 16:16:54 +0800

    .

--- publications/cv.js
+++ publications/cv.js
@@ -99,7 +99,7 @@ function cvrender() {
     cv.forEach((item, _) => {
         var innerHTML = `
             <a href="${item.link}" target="_blank" class="hover:opacity-70 flex flex-row laptop:flex-col justify-center">
-                <img loading="lazy" src="${item.image}" class="w-fulls laptop:w-96"/>
+                <img loading="lazy" src="${item.image}" class="w-full laptop:w-96"/>
             </a>
             <div class="flex flex-col justify-center flex-1">
                 <h3>
