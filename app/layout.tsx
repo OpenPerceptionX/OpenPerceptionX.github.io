d10b1f2827649df8afc27ebf22e6d13115a219e3
faikit <huijie.wang@hotmail.com>
2025-05-24 23:32:05 +0800

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
-import type { Metadata } from "next";
 import * as React from "react"
+import type { Metadata } from "next";
 import Link from "next/link"
+import Image from 'next/image'
+
+
+
 import { Button } from "@/components/ui/button"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
 
 
 
 import "./globals.css";
-import Image from 'next/image'
 
 
 
@@ -21,12 +26,18 @@ export const metadata: Metadata = {
         ],
     },
 };
+
+
+
 // font
 import { Inter } from "next/font/google";
 const inter = Inter({
     subsets: ["latin"],
     display: "swap",
 });
+
+
+
 // Google Analystics
 import { GoogleAnalytics } from '@next/third-parties/google'
 
@@ -43,41 +54,62 @@ export default function RootLayout({
 
 
             <body id="#">       
-                <header>
+                <SidebarProvider>
 
 
 
-                    <div className="fixed flex flex-row gap-3 m-6 z-20 flex-wrap">
-                        <Button asChild className="bg-background text-foreground hover:bg-white group">
-                            <Link href="/" className="select-none">
-                                <Image
-                                    src="/logo/OpenDriveLab/D.png"
-                                    alt="OpenDriveLab"
-                                    width={24}
-                                    height={24}
-                                    className="group-hover:scale-125 transition delay-100 duration-200"
-                                />
+                    <header>
+
+
+
+                        <AppSidebar />
+
+
+
+                        <div className="fixed flex flex-row gap-3 m-6 z-100 flex-wrap">
+
+                            <Button asChild className="bg-background text-foreground hover:bg-white group">
+                                <Link href="/" className="select-none">
+                                    <Image
+                                        src="/logo/OpenDriveLab/D.png"
+                                        alt="OpenDriveLab"
+                                        width={24}
+                                        height={24}
+                                        className="group-hover:scale-125 transition delay-100 duration-200"
+                                    />
+                                </Link>
+                            </Button>
+
+                            <SidebarTrigger />
+
+                        </div>
+
+
+
+                        <div className="fixed right-0 bottom-0 m-6 z-20">
+                            <Link href="#" className="bg-background text-foreground hover:bg-o-blue hover:text-background rounded-full flex justify-center items-center p-2 select-none">
+                                <span>
+                                    <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 18.75 7.5-7.5 7.5 7.5" />
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 7.5-7.5 7.5 7.5" />
+                                    </svg>
+                                </span>
                             </Link>
-                        </Button>
-                    </div>
+                        </div>
 
 
 
-                    <div className="fixed right-0 bottom-0 m-6 z-20">
-                        <Link href="#" className="bg-background text-foreground hover:bg-o-blue hover:text-background rounded-full flex justify-center items-center p-2 select-none">
-                            <span>
-                                <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 18.75 7.5-7.5 7.5 7.5" />
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 7.5-7.5 7.5 7.5" />
-                                </svg>
-                            </span>
-                        </Link>
-                    </div>
+                    </header>
 
 
 
-                </header>
-                {children}
+                    <main>
+                        {children}
+                    </main>
+
+
+
+                </SidebarProvider>
             </body>
 
 
