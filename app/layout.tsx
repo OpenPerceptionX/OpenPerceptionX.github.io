ddd1641a9c2d38d44e0a454670c752c860d07af6
faikit <huijie.wang@hotmail.com>
2025-05-24 22:15:52 +0800

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,17 @@ import Image from 'next/image'
 
 
 
+// icon
+export const metadata: Metadata = {
+    icons: {
+        icon: [
+            {
+                url: "/logo/OpenDriveLab/D.png",
+                href: "/logo/OpenDriveLab/D.png",
+            },
+        ],
+    },
+};
 // font
 import { Inter } from "next/font/google";
 const inter = Inter({
@@ -21,20 +32,6 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 
 
 
-// icon
-export const metadata: Metadata = {
-    icons: {
-        icon: [
-            {
-                url: "/logo/OpenDriveLab/D_small.png",
-                href: "/logo/OpenDriveLab/D_small.png",
-            },
-        ],
-    },
-};
-
-
-
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -42,10 +39,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" className={inter.className}>
-            <body id="#">
 
 
-                                        
+
+            <body id="#">       
                 <header>
 
 
@@ -56,8 +53,8 @@ export default function RootLayout({
                                 <Image
                                     src="/logo/OpenDriveLab/D.png"
                                     alt="OpenDriveLab"
-                                    width={20}
-                                    height={20}
+                                    width={24}
+                                    height={24}
                                     className="group-hover:scale-125 transition delay-100 duration-200"
                                 />
                             </Link>
@@ -67,7 +64,7 @@ export default function RootLayout({
 
 
                     <div className="fixed right-0 bottom-0 m-6 z-20">
-                        <Link href="#" className="bg-background text-foreground hover:bg-mred hover:text-background rounded-full flex justify-center items-center p-2 select-none">
+                        <Link href="#" className="bg-background text-foreground hover:bg-o-blue hover:text-background rounded-full flex justify-center items-center p-2 select-none">
                             <span>
                                 <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 18.75 7.5-7.5 7.5 7.5" />
@@ -80,15 +77,15 @@ export default function RootLayout({
 
 
                 </header>
-
-
-
                 {children}
-                
-
-
             </body>
+
+
+
             <GoogleAnalytics gaId="G-L7VEDHS6G8" />
+
+
+
         </html>
     );
 }
