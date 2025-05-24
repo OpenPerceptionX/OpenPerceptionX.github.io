c3a44aae4a62d2e2fa9c93e655710607f18e1332
faikit <huijie.wang@hotmail.com>
2025-05-25 02:34:17 +0800

    .

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 export const metadata: Metadata = {
-    title: "OpenDriveLab",
+    title: "redirecting",
 };
 
 
--- components/ui/404.tsx
+++ components/ui/404.tsx
@@ -227,21 +227,8 @@ export default function NotFound404() {
     }
 
 
-
+    redirect('/missing')
     return (
-        <main className='w-full'>
-            <div className="w-full h-svh flex flex-row justify-center items-center">
-                <div className='w-full md:w-3/4 h-full md:h-3/4 md:rounded-sm bg-gradient-landing flex flex-row justify-center items-center'>
-                    <div className="md:w-full max-w-3xl px-6 flex flex-col gap-6 select-none ">
-                        <h1 className="font-bold text-t1 text-white">
-                            404
-                        </h1>
-                        <h1 className="font-bold text-t1 text-white">
-                            Not Found
-                        </h1>
-                    </div>
-                </div>
-            </div>
-        </main>
+        <main className='w-full'></main>
     )
 }
