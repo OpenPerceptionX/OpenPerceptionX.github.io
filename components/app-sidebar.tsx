2b1ca3dac61d8dc5a9f779ffbba7cb1c67a6295f
faikit <huijie.wang@hotmail.com>
2025-05-25 01:00:19 +0800

    .

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,15 +1,151 @@
+"use client"
+
+
 import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarHeader,
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuItem,
+    SidebarMenuButton,
+    SidebarMenuSubItem,
+    SidebarMenuSub,
 } from "@/components/ui/sidebar"
- 
+import { useSidebar } from "@/components/ui/sidebar"
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible"
+import { ChevronDown  } from "lucide-react"
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+
+
+import { FadeIn } from "@/components/animation/fade-in"
+
+
+
 export function AppSidebar() {
-  return (
-    <Sidebar>
-      <SidebarContent />
-    </Sidebar>
-  )
+    const {
+        state,
+        open,
+        setOpen,
+        openMobile,
+        setOpenMobile,
+        isMobile,
+        toggleSidebar,
+    } = useSidebar()
+    return (
+        <FadeIn>
+            <Sidebar>
+                <SidebarMenu className="mt-24 px-8 text-sm">
+
+
+
+                    <Collapsible defaultOpen className="group/collapsible flex flex-col gap-6">
+                        <SidebarMenuItem>
+                            <CollapsibleTrigger className="w-full flex flex-row justify-between items-center p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                                Project
+                                <ChevronDown className="size-4 transition-transform group-data-[state=closed]/collapsible:rotate-270 group-data-[state=open]/collapsible:rotate-0" />
+                            </CollapsibleTrigger>
+                            <CollapsibleContent>
+                                <SidebarMenuSub>
+                                    <Link href="/blog/agibot-world" className="p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                                        AgiBot World
+                                    </Link>
+                                    <Link href="/vista" target="_blank" className="p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                                        OpenDV
+                                    </Link>
+                                    <Link href="/drivelm" target="_blank" className="p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                                        DriveLM
+                                    </Link>
+                                    <Link href="/dataset"  className="p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                                        more
+                                    </Link>
+                                </SidebarMenuSub>
+                            </CollapsibleContent>
+                        </SidebarMenuItem>
+                    </Collapsible>
+
+
+
+                    <Collapsible defaultOpen className="group/collapsible flex flex-col gap-6">
+                        <SidebarMenuItem>
+                            <CollapsibleTrigger className="w-full flex flex-row justify-between items-center p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                                Publication
+                                <ChevronDown className="size-4 transition-transform group-data-[state=closed]/collapsible:rotate-270 group-data-[state=open]/collapsible:rotate-0" />
+                            </CollapsibleTrigger>
+                            <CollapsibleContent>
+                                <SidebarMenuSub>
+                                    <Link href="https://github.com/OpenDriveLab/UniAD" className="p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                                        UniAD
+                                    </Link>
+                                    <Link href="https://github.com/OpenDriveLab/UniVLA" target="_blank" className="p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                                        UniVLA
+                                    </Link>
+                                    <Link href="https://www.computer.org/csdl/journal/tp/2025/03/10791908/22ABgP6PlUQ" target="_blank" className="p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                                        BEVFormer
+                                    </Link>
+                                    <Link href="/publications"  className="p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                                        more
+                                    </Link>
+                                </SidebarMenuSub>
+                            </CollapsibleContent>
+                        </SidebarMenuItem>
+                    </Collapsible>
+
+
+
+                    <Collapsible defaultOpen className="group/collapsible flex flex-col gap-6">
+                        <SidebarMenuItem>
+                            <CollapsibleTrigger className="w-full flex flex-row justify-between items-center p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                                Event
+                                <ChevronDown className="size-4 transition-transform group-data-[state=closed]/collapsible:rotate-270 group-data-[state=open]/collapsible:rotate-0" />
+                            </CollapsibleTrigger>
+                            <CollapsibleContent>
+                                <SidebarMenuSub>
+                                    <Link href="/challenge2025" className="p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                                        Challenge 2025
+                                    </Link>
+                                    <Link href="/cvpr2025" className="p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                                        Events at CVPR 2025
+                                    </Link>
+                                    <Link href="https://sagroups.ieee.org/3474/" target="_blank" className="p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                                        IEEE Standard P3474
+                                    </Link>
+                                    <Link href="/events"  className="p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                                        more
+                                    </Link>
+                                </SidebarMenuSub>
+                            </CollapsibleContent>
+                        </SidebarMenuItem>
+                    </Collapsible>
+
+
+
+                    <Link href="/team"  className="p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                        Team
+                    </Link>
+
+
+
+                    <Link href="/recruit"  className="p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                        Recruit
+                    </Link>
+
+
+
+                    <Link href="https://github.com/sponsors/OpenDriveLab" target="_blank" className="p-2 rounded-sm hover:bg-o-blue/3 select-none">
+                        Sponsor
+                    </Link>
+
+
+
+                </SidebarMenu>
+            </Sidebar>
+        </FadeIn>
+    )
 }
--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -643,7 +643,7 @@ function SidebarMenuSub({ className, ...props }: React.ComponentProps<"ul">) {
       data-slot="sidebar-menu-sub"
       data-sidebar="menu-sub"
       className={cn(
-        "border-sidebar-border mx-3.5 flex min-w-0 translate-x-px flex-col gap-1 border-l px-2.5 py-0.5",
+        "border-sidebar-border text-foreground/50 ml-2 flex min-w-0 translate-x-px flex-col gap-.5 border-l pl-2",
         "group-data-[collapsible=icon]:hidden",
         className
       )}
