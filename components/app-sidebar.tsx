7c899563844420e358e733f63fc3b10c6d2e076e
faikit <huijie.wang@hotmail.com>
2025-05-24 23:32:05 +0800

    .

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,15 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar"
+ 
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent />
+    </Sidebar>
+  )
+}
