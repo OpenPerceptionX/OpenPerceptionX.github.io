9b36fe2e29fbad001fe0c856eca6d7f8808c6f52
faikit <huijie.wang@hotmail.com>
2025-05-25 02:07:09 +0800

    .

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 export const metadata: Metadata = {
     title: "OpenDriveLab",
-    description: "404 Not Found",
-    keywords: ["404", "OpenDriveLab", "HKU", "SII"],
 };
 
 
