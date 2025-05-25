f57f8692a34b0dfe4c0c3ac777abe328c18d83ab
faikit <huijie.wang@hotmail.com>
2025-05-26 00:55:18 +0800

    .

--- app/(project)/FreeTacMan/page.tsx
+++ app/(project)/FreeTacMan/page.tsx
@@ -128,7 +128,7 @@ export default function Home() {
 
           <div className="w-full pl-6 pr-6 flex flex-col items-center" id="highlights">
             <div className="w-full flex flex-col items-center mt-16">
-              <div className="w-full max-w-5xl flex flex-row justify-between">
+              <div className="w-full max-w-4xl flex flex-row justify-between">
                 <h2 className="text-3xl font-bold">
                   <Link
                     href="#highlights"
@@ -164,7 +164,7 @@ export default function Home() {
           <div className="w-full pl-6 pr-6 flex flex-col items-center">
             <div className="w-full pl-6 pr-6 flex flex-col items-center">
               <div className="w-full flex flex-col items-center mt-20">
-                <div className="w-full max-w-3xl flex flex-row justify-between">
+                <div className="w-full max-w-4xl flex flex-row justify-between">
                   <div className="flex flex-col gap-6">
                     <p className="text-base text-left w-full">
                       Enabling robots with contact-rich manipulation remains a
@@ -211,7 +211,7 @@ export default function Home() {
 
           <div className="w-full pl-6 pr-6 flex flex-col items-center">
             <div className="w-full pl-6 pr-6 flex flex-col items-center">
-              <div className="w-full max-w-5xl mt-20">
+              <div className="w-full max-w-6xl mt-20">
                 <Tabs
                   defaultValue="01"
                   className="flex flex-row w-full gap-6 h-[576px]"
@@ -289,7 +289,7 @@ export default function Home() {
 
           <div className="w-full pl-6 pr-6 flex flex-col items-center">
             <div className="w-full pl-6 pr-6 flex flex-col items-center">
-              <Separator className="max-w-5xl mt-28" />
+              <Separator className="max-w-4xl mt-28" />
             </div>
           </div>
 
@@ -299,7 +299,7 @@ export default function Home() {
           {/* FreeTacMan 3D Model  */}
           <div className="w-full pl-6 pr-6 flex flex-col items-center" id="model">
             <div className="w-full flex flex-col items-center mt-16">
-              <div className="w-full max-w-5xl flex flex-row justify-between">
+              <div className="w-full max-w-4xl flex flex-row justify-between">
                 <h2 className="text-3xl font-bold">
                   <Link
                     href="#model"
@@ -335,13 +335,13 @@ export default function Home() {
 
           <div className="w-full pl-6 pr-6 flex flex-col items-center">
             <div className="my-10 flex flex-col items-center pl-6 pr-6 inset-0 w-full">
-              <div className="w-full max-w-5xl my-5">
+              <div className="w-full max-w-4xl my-5">
                 <div className="flex flex-col gap-5 h-full">
                   <div className="flex flex-col justify-center gap-5">
                     <span className="font-bold text-2xl text-center">
                       Interactive Modal Viewer
                     </span>
-                    <p className="text-base text-center max-w-5xl">
+                    <p className="text-base text-center max-w-6xl">
                       Dive into our 💡
                       <b className="text-[#004cee]">interactive</b> 3D model
                       viewer and explore the most popular native 3D formats with
@@ -372,7 +372,7 @@ export default function Home() {
                         <source src="https://opendrivelab.github.io/FreeTacMan/3d_model/piperandfranka.mp4" />
                       </video>
                       <div className="w-full flex justify-center">
-                        <div className="max-w-3xl">
+                        <div className="max-w-4xl">
                           <p className="text-base text-left w-full">
                             FreeTacMan features a universal gripper interface
                             with quick-swap mounts compatible with various
@@ -399,7 +399,7 @@ export default function Home() {
 
 
           <div className="w-full pl-6 pr-6 flex flex-col items-center">
-            <Separator className="max-w-5xl mt-68" />
+            <Separator className="max-w-4xl mt-68" />
           </div>
 
 
@@ -408,7 +408,7 @@ export default function Home() {
           {/* Application  */}
           <div className="w-full pl-6 pr-6 flex flex-col items-center" id="applications">
             <div className="w-full flex flex-col items-center mt-16">
-              <div className="w-full max-w-5xl flex flex-row justify-between">
+              <div className="w-full max-w-4xl flex flex-row justify-between">
                 <h2 className="text-3xl font-bold">
                   <Link
                     href="#applications"
@@ -442,7 +442,7 @@ export default function Home() {
           <div className="w-full pl-6 pr-6 flex flex-col items-center">
             <div className="w-full pl-6 pr-6 flex flex-col items-center">
               <div className="w-full flex flex-col items-center mt-20">
-                <div className="w-full max-w-3xl flex flex-row justify-between">
+                <div className="w-full max-w-4xl flex flex-row justify-between">
                   <p className="text-base text-left w-full">
                     We evaluate the effectiveness of FreeTacMan system and the
                     quality of the collected visuo-tactile demonstration through
@@ -458,7 +458,7 @@ export default function Home() {
 
           <div className="w-full pl-6 pr-6 flex flex-col items-center">
             <div className="w-full pl-6 pr-6 flex flex-col items-center">
-              <div className="w-full max-w-5xl mt-20">
+              <div className="w-full max-w-6xl mt-20">
                 {/* <div className="bg-[#98AEF3] mt-20 p-10">
                                 <div className="flex w-full justify-center border-8 bg-[#eaeaea]">
                                     <span className="text-3xl font-bold text-center p-10  select-none">FreeTacMan</span>
@@ -572,7 +572,7 @@ export default function Home() {
 
 
           <div className="w-full pl-6 pr-6 flex flex-col items-center">
-            <Separator className="max-w-5xl mt-24" />
+            <Separator className="max-w-4xl mt-24" />
           </div>
 
 
@@ -581,7 +581,7 @@ export default function Home() {
           {/* Policy Rollouts */}
           <div className="w-full pl-6 pr-6 flex flex-col items-center" id="policy-rollouts" >
             <div className="w-full flex flex-col items-center mt-16">
-              <div className="w-full max-w-5xl flex flex-row justify-between gap-20 items-center">
+              <div className="w-full max-w-4xl flex flex-row justify-between gap-20 items-center">
                 <h2 className="text-3xl font-bold">
                   <Link
                     href="#policy-rollouts"
@@ -618,7 +618,7 @@ export default function Home() {
           <div className="w-full pl-6 pr-6 flex flex-col items-center">
             <div className="w-full pl-6 pr-6 flex flex-col items-center">
               <div className="w-full flex flex-col items-center mt-20">
-                <div className="w-full max-w-3xl flex flex-row justify-between">
+                <div className="w-full max-w-4xl flex flex-row justify-between">
                   <p className="text-base text-left w-full">
                     We integrate{" "}
                     <b className="text-[#004cee]">tactile feedback</b> to assess
@@ -636,11 +636,18 @@ export default function Home() {
                     vision-only counterparts.
                   </p>
                 </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full pl-6 pr-6 flex flex-col items-center">
+            <div className="w-full pl-6 pr-6 flex flex-col items-center">
+              <div className="w-full flex flex-col items-center mt-20">
                 <div className="w-full flex justify-center mt-10">
                   <img
                     src="https://opendrivelab.github.io/FreeTacMan/policy_rollouts/policy rollouts_results.png"
                     alt="Policy Rollouts Results"
-                    className="max-w-3xl w-full"
+                    className="max-w-6xl w-full"
                   />
                 </div>
               </div>
@@ -651,7 +658,7 @@ export default function Home() {
 
 
           <div className="w-full pl-6 pr-6 flex flex-col items-center">
-            <div className="w-full max-w-5xl mt-20">
+            <div className="w-full max-w-4xl mt-20">
               <Tabs
                 defaultValue={policy_rollouts[0].id}
                 className="flex flex-col w-full"
@@ -895,7 +902,7 @@ export default function Home() {
 
 
           <div className="w-full pl-6 pr-6 flex flex-col items-center">
-            <Separator className="max-w-5xl mt-28" />
+            <Separator className="max-w-4xl mt-28" />
           </div>
 
 
@@ -905,7 +912,7 @@ export default function Home() {
           {/* User Study */}
           <div className="w-full pl-6 pr-6 flex flex-col items-center" id="user-study" >
             <div className="w-full flex flex-col items-center mt-16">
-              <div className="w-full max-w-5xl flex flex-row justify-between items-center gap-20">
+              <div className="w-full max-w-4xl flex flex-row justify-between items-center gap-20">
                 <h2 className="text-3xl font-bold">
                   <Link
                     href="#user-study"
@@ -942,7 +949,7 @@ export default function Home() {
           <div className="w-full pl-6 pr-6 flex flex-col items-center">
             <div className="w-full pl-6 pr-6 flex flex-col items-center">
               <div className="w-full flex flex-col items-center mt-20">
-                <div className="w-full max-w-3xl flex flex-row justify-between">
+                <div className="w-full max-w-4xl flex flex-row justify-between">
                   <p className="text-base text-left w-full">
                     We evaluate the usability of FreeTacMan through a user study
                     involving 12 human participants with varying levels of
@@ -970,7 +977,7 @@ export default function Home() {
               <img
                 src="https://opendrivelab.github.io/FreeTacMan/user_study/userstudy_results.png"
                 alt="User Study Results"
-                className="max-w-5xl w-full rounded-xl"
+                className="max-w-6xl w-full rounded-xl"
               />
             </div>
             <div className="w-full max-w-[1400px] px-20 3xl:px-64 mt-10 p-6 overflow-hidden">
@@ -1173,7 +1180,7 @@ export default function Home() {
 
 
           <div className="w-full pl-6 pr-6 flex flex-col items-center">
-            <Separator className="max-w-5xl mt-32" />
+            <Separator className="max-w-4xl mt-32" />
           </div>
 
 
@@ -1181,7 +1188,7 @@ export default function Home() {
 
       <footer className="w-full mb-24 flex flex-col gab-10">
         <div className="w-full pl-6 pr-6 flex flex-row justify-center mt-24">
-          <div className="max-w-5xl w-full flex flex-row justify-between">
+          <div className="max-w-4xl w-full flex flex-row justify-between">
             <div className="flex-1 flex flex-col justify-start">
               <span className="select-none">FreeTacMan © 2025</span>
             </div>
