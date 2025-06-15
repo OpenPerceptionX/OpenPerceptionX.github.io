6a7507c72780d6752c0991b2b6f830c4ff659fa1
faikit <huijie.wang@hotmail.com>
2025-06-15 23:44:11 +0800

    .

--- app/(default)/(page)/tttt/page.tsx
+++ app/(default)/(page)/tttt/page.tsx
@@ -7,21 +7,18 @@ export const metadata: Metadata = {
 
 
 
-import * as React from "react"
-
-
-
 import Image from 'next/image'
 import Link from "next/link"
-import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-} from "@/components/ui/carousel"
+
+
+
 import { AspectRatio } from "@/components/ui/aspect-ratio"
 import { Separator } from "@/components/ui/separator"
+
+
+
+import { members } from "@/data/team"
+
 
 
 export default function Home() {
@@ -44,6 +41,52 @@ export default function Home() {
                             </span>
                         </Link>
                     </h2>
+                </div>
+            </div>
+
+
+
+            <div className="w-full px-6 flex justify-center mt-24">
+                <div className="w-full max-w-7xl grid gap-12 grid-cols-2 md:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5">
+                    {[...members.values()].filter(member => member.keys.includes("scholar")).map((member) => (
+                        <div className="flex flex-col gap-3 group/image" key={member.name}>
+                            <AspectRatio ratio={1/1}>
+                                <Image
+                                    src={member.image}
+                                    alt={member.name}
+                                    fill
+                                    className="h-full w-full rounded-sm object-cover bg-gradient-landing group-hover/image:hidden"
+                                />
+                                <Image
+                                    src={member.image_s}
+                                    alt={member.name}
+                                    fill
+                                    className="h-full w-full rounded-sm object-cover bg-gradient-landing hidden group-hover/image:block"
+                                />
+                            </AspectRatio>
+                            <b className="text-xl">
+                                {member.name}
+                            </b>
+                            <span className="text-xs">
+                                {member.desc}
+                            </span>
+                            <div className="flex gap-3 flex-wrap">
+                                {member.links.map((link) => (
+                                    <Link href={link.url} target="_blank" key={link.icon} className="size-4 md:size-5 group/icon">
+                                        <AspectRatio ratio={1/1}>
+                                            <Image
+                                                src={"/icon/" + link.icon + ".svg"}
+                                                alt={link.icon}
+                                                fill
+                                                className="group-hover/icon:scale-125 transition duration-200"
+                                            />
+                                        </AspectRatio>
+                                    </Link>
+                                ))}
+                            </div>
+
+                        </div>
+                    ))}
                 </div>
             </div>
 
@@ -74,6 +117,52 @@ export default function Home() {
 
 
             <div className="w-full px-6 flex justify-center mt-24">
+                <div className="w-full max-w-7xl grid gap-12 grid-cols-2 md:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5">
+                    {[...members.values()].filter(member => member.keys.includes("phd")).map((member) => (
+                        <div className="flex flex-col gap-3 group/image" key={member.name}>
+                            <AspectRatio ratio={1/1}>
+                                <Image
+                                    src={member.image}
+                                    alt={member.name}
+                                    fill
+                                    className="h-full w-full rounded-sm object-cover bg-gradient-landing group-hover/image:hidden"
+                                />
+                                <Image
+                                    src={member.image_s}
+                                    alt={member.name}
+                                    fill
+                                    className="h-full w-full rounded-sm object-cover bg-gradient-landing hidden group-hover/image:block"
+                                />
+                            </AspectRatio>
+                            <b className="text-xl">
+                                {member.name}
+                            </b>
+                            <span className="text-xs">
+                                {member.desc}
+                            </span>
+                            <div className="flex gap-3 flex-wrap">
+                                {member.links.map((link) => (
+                                    <Link href={link.url} target="_blank" key={link.icon} className="size-4 md:size-5 group/icon">
+                                        <AspectRatio ratio={1/1}>
+                                            <Image
+                                                src={"/icon/" + link.icon + ".svg"}
+                                                alt={link.icon}
+                                                fill
+                                                className="group-hover/icon:scale-125 transition duration-200"
+                                            />
+                                        </AspectRatio>
+                                    </Link>
+                                ))}
+                            </div>
+
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+
+
+            <div className="w-full px-6 flex justify-center mt-24">
                 <Separator className="max-w-7xl"/>
             </div>
 
@@ -84,6 +173,76 @@ export default function Home() {
                     <h2 className="text-t1 font-bold"> 
                         <Link href="#intern" className="scroll-mt-32 group flex items-center" id="intern">
                             Intern
+                            <span className="ml-6 hidden group-hover:inline-block size-6 text-foreground">
+                                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor">
+                                    <path d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212" strokeWidth="2.5" strokeLinecap="round"/>
+                                    <path d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373" strokeWidth="2.5" strokeLinecap="round"/>
+                                </svg>
+                            </span>
+                        </Link>
+                    </h2>
+                </div>
+            </div>
+
+
+
+            <div className="w-full px-6 flex justify-center mt-24">
+                <div className="w-full max-w-7xl grid gap-12 grid-cols-2 md:grid-cols-3 xl:grid-cols-4 3xl:grid-cols-5">
+                    {[...members.values()].filter(member => member.keys.includes("intern")).map((member) => (
+                        <div className="flex flex-col gap-3 group/image" key={member.name}>
+                            <AspectRatio ratio={1/1}>
+                                <Image
+                                    src={member.image}
+                                    alt={member.name}
+                                    fill
+                                    className="h-full w-full rounded-sm object-cover bg-gradient-landing group-hover/image:hidden"
+                                />
+                                <Image
+                                    src={member.image_s}
+                                    alt={member.name}
+                                    fill
+                                    className="h-full w-full rounded-sm object-cover bg-gradient-landing hidden group-hover/image:block"
+                                />
+                            </AspectRatio>
+                            <b className="text-xl">
+                                {member.name}
+                            </b>
+                            <span className="text-xs">
+                                {member.desc}
+                            </span>
+                            <div className="flex gap-3 flex-wrap">
+                                {member.links.map((link) => (
+                                    <Link href={link.url} target="_blank" key={link.icon} className="size-4 md:size-5 group/icon">
+                                        <AspectRatio ratio={1/1}>
+                                            <Image
+                                                src={"/icon/" + link.icon + ".svg"}
+                                                alt={link.icon}
+                                                fill
+                                                className="group-hover/icon:scale-125 transition duration-200"
+                                            />
+                                        </AspectRatio>
+                                    </Link>
+                                ))}
+                            </div>
+
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+
+
+            <div className="w-full px-6 flex justify-center mt-24">
+                <Separator className="max-w-7xl"/>
+            </div>
+
+
+
+            <div className="w-full px-6 flex justify-center mt-24">
+                <div className="w-full max-w-7xl flex">
+                    <h2 className="text-t1 font-bold"> 
+                        <Link href="#alumni" className="scroll-mt-32 group flex items-center" id="alumni">
+                            Alumni
                             <span className="ml-6 hidden group-hover:inline-block size-6 text-foreground">
                                 <svg viewBox="0 0 24 24" fill="none" stroke="currentColor">
                                     <path d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212" strokeWidth="2.5" strokeLinecap="round"/>
---
+++ data/team.tsx
@@ -0,0 +1,539 @@
+export const members: {
+    name: string;
+    image: string;
+    image_s: string,
+    desc: string;
+    links: { icon: string; url: string }[];
+    keys: string[];
+}[] = [
+    {
+        name: "Hongyang Li",
+        image: "/assets/person/hongyang_li.jpg",
+        image_s: "/assets/person/hongyang_li_s.jpg",
+        desc: "Autonomous Driving & Embodied AI",
+        links: [
+            {
+                icon: "homepage",
+                url: "https://lihongyang.info/",
+            },
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=Hfrih1EAAAAJ",
+            },
+            {
+                icon: "email",
+                url: "mailto:hy@opendrivelab.com",
+            },
+        ],
+        keys: ["scholar"],
+    },
+    {
+        name: "Li Chen",
+        image: "/assets/person/li_chen.jpg",
+        image_s: "/assets/person/li_chen_s.jpg",
+        desc: "End-to-End Autonomous Driving & Embodied AI",
+        links: [
+            {
+                icon: "homepage",
+                url: "https://ilnehc.github.io/",
+            },
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=ulZxvY0AAAAJ",
+            },
+            {
+                icon: "email",
+                url: "mailto:lichen@opendrivelab.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/ilnehc",
+            },
+            {
+                icon: "x",
+                url: "https://twitter.com/ilnehc",
+            },
+        ],
+        keys: ["scholar"],
+    },
+    {
+        name: "Huijie Wang",
+        image: "/assets/person/huijie_wang.jpg",
+        image_s: "/assets/person/huijie_wang_s.jpg",
+        desc: "Ecosystem",
+        links: [
+            {
+                icon: "homepage",
+                url: "https://faikit.github.io/",
+            },
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=Xg4cp-EAAAAJ",
+            },
+            {
+                icon: "email",
+                url: "mailto:huijie.wang@opendrivelab.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/faikit",
+            },
+            {
+                icon: "x",
+                url: "https://twitter.com/wongfaikit",
+            },
+        ],
+        keys: ["scholar"],
+    },
+    {
+        name: "Chonghao Sima",
+        image: "/assets/person/chonghao_sima.jpg",
+        image_s: "/assets/person/chonghao_sima_s.jpg",
+        desc: "Autonomous Driving",
+        links: [
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=dgYJ6esAAAAJ",
+            },
+            {
+                icon: "email",
+                url: "mailto:smch@opendrivelab.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/ChonghaoSima",
+            },
+            {
+                icon: "x",
+                url: "https://twitter.com/smch_1127",
+            },
+        ],
+        keys: ["scholar"],
+    },
+    {
+        name: "Tianyu Li",
+        image: "/assets/person/tianyu_li.jpg",
+        image_s: "/assets/person/tianyu_li_s.jpg",
+        desc: "復旦大學（23級聯培）",
+        links: [
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=X6vTmEMAAAAJ",
+            },
+            {
+                icon: "email",
+                url: "mailto:litianyu@opendrivelab.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/sephyli",
+            },
+            {
+                icon: "x",
+                url: "https://twitter.com/sephy_li",
+            },
+        ],
+        keys: ["phd"],
+    },
+    {
+        name: "Qingwen Bu",
+        image: "/assets/person/qingwen_bu.jpg",
+        image_s: "/assets/person/qingwen_bu_s.jpg",
+        desc: "上海交通大學（23級聯培）",
+        links: [
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=-JCRysgAAAAJ",
+            },
+            {
+                icon: "email",
+                url: "mailto:qwbu01@sjtu.edu.cn",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/retsuh-bqw",
+            },
+        ],
+        keys: ["phd"],
+    },
+    {
+        name: "Hanxue Zhang",
+        image: "/assets/person/hanxue_zhang.jpg",
+        image_s: "/assets/person/hanxue_zhang_s.jpg",
+        desc: "上海交通大學（24級聯培）",
+        links: [
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=vy5r8rMAAAAJ",
+            },
+            {
+                icon: "email",
+                url: "mailto:snow@opendrivelab.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/jjxjiaxue",
+            },
+        ],
+        keys: ["phd"],
+    },
+    {
+        name: "Chengen Xie",
+        image: "/assets/person/chengen_xie.jpg",
+        image_s: "/assets/person/chengen_xie_s.jpg",
+        desc: "上海交通大學（24級聯培）",
+        links: [
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=-Sk1x_gAAAAJ",
+            },
+            {
+                icon: "email",
+                url: "mailto:xiechengen@opendrivelab.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/ChengenXie",
+            },
+        ],
+        keys: ["phd"],
+    },
+    {
+        name: "Haoran Jiang",
+        image: "/assets/person/haoran_jiang.jpg",
+        image_s: "/assets/person/haoran_jiang_s.jpg",
+        desc: "復旦大學（24級聯培）",
+        links: [
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=KYzm-k0AAAAJ",
+            },
+            {
+                icon: "email",
+                url: "mailto:jianghaoran2024@gmail.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/HRRiver7",
+            },
+        ],
+        keys: ["phd"],
+    },
+    {
+        name: "Yihang Qiu",
+        image: "/assets/person/yihang_qiu.jpg",
+        desc: "The University of Hong Kong (HKU)",
+        image_s: "/assets/person/yihang_qiu_s.jpg",
+        links: [
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=qgRUOdIAAAAJ",
+            },
+            {
+                icon: "email",
+                url: "mailto:qiuyihang@opendrivelab.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/GihhArwtw",
+            },
+        ],
+        keys: ["intern"],
+    },
+    {
+        name: "Yixuan Pan",
+        image: "/assets/person/yixuan_pan.jpg",
+        desc: "The University of Hong Kong (HKU)",
+        image_s: "/assets/person/yixuan_pan_s.jpg",
+        links: [
+            {
+                icon: "homepage",
+                url: "https://lzpyx.github.io/",
+            },
+            {
+                icon: "email",
+                url: "mailto:yixuanpan2024@gmail.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/lzpyx",
+            },
+        ],
+        keys: ["intern"],
+    },
+    {
+        name: "Jiazhi Yang",
+        image: "/assets/person/jiazhi_yang.jpg",
+        desc: "The Chinese University of Hong Kong (CUHK)",
+        image_s: "/assets/person/jiazhi_yang_s.jpg",
+        links: [
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=Ju7nGX8AAAAJ",
+            },
+            {
+                icon: "email",
+                url: "mailto:yangjiazhi@opendrivelab.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/YTEP-ZHI",
+            },
+            {
+                icon: "x",
+                url: "https://x.com/jiazhi_yang2024",
+            },
+        ],
+        keys: ["intern"],
+    },
+    {
+        name: "Yunsong Zhou",
+        image: "/assets/person/yunsong_zhou.jpg",
+        desc: "Shanghai Jiao Tong University (SJTU)",
+        image_s: "/assets/person/yunsong_zhou_s.jpg",
+        links: [
+            {
+                icon: "homepage",
+                url: "https://zhouyunsong.github.io/",
+            },
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=bTsmnwcAAAAJ",
+            },
+            {
+                icon: "email",
+                url: "mailto:zhouyunsong@opendrivelab.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/ZhouYunsong",
+            },
+        ],
+        keys: ["intern"],
+    },
+    {
+        name: "Longyan Wu",
+        image: "/assets/person/longyan_wu.jpg",
+        desc: "Fudan University",
+        image_s: "/assets/person/longyan_wu_s.jpg",
+        links: [
+            {
+                icon: "email",
+                url: "mailto:im.longyanwu@gmail.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/wly-Lilie",
+            },
+        ],
+        keys: ["intern"],
+    },
+    {
+        name: "Haochen Tian",
+        image: "/assets/person/haochen_tian.jpg",
+        desc: "Institute of Automation, Chinese Academy of Sciences (CASIA)",
+        image_s: "/assets/person/haochen_tian_s.jpg",
+        links: [
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=DoyuNwEAAAAJ",
+            },
+            {
+                icon: "email",
+                url: "mailto:tianhaochen2023@ia.ac.cn",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/hctian713",
+            },
+        ],
+        keys: ["intern"],
+    },
+    {
+        name: "Modi Shi",
+        image: "/assets/person/modi_shi.jpg",
+        desc: "Beijing University of Aeronautics and Astronautics (BUAA)",
+        image_s: "/assets/person/modi_shi_s.jpg",
+        links: [
+            {
+                icon: "email",
+                url: "mailto:modishi@buaa.edu.cn",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/ModiShi",
+            },
+        ],
+        keys: ["intern"],
+    },
+    {
+        name: "Jin Chen",
+        image: "/assets/person/jin_chen.jpg",
+        desc: "Xi'an Jiaotong University (XJTU)",
+        image_s: "/assets/person/jin_chen_s.jpg",
+        // desc: "复旦大学（25级联培）",
+        links: [
+            {
+                icon: "email",
+                url: "mailto:cj65000816081@gmail.com",
+            },
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=4FqHXOsAAAAJ",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/dunknsabsw",
+            },
+            {
+                icon: "zhihu",
+                url: "https://www.zhihu.com/people/lu-kou-5-68",
+            },
+            {
+                icon: "xiaohongshu",
+                url: "https://www.xiaohongshu.com/user/profile/609cbe510000000001002922",
+            },
+        ],
+        keys: ["intern"],
+    },
+    {
+        name: "Jisong Cai",
+        image: "/assets/person/jisong_cai.jpg",
+        desc: "Wuhan University",
+        image_s: "/assets/person/jisong_cai_s.jpg",
+        // desc: "上海交通大学（25级联培）",
+        links: [
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=dTrpq94AAAAJ",
+            },
+            {
+                icon: "email",
+                url: "mailto:serene.js.cai@gmail.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/serene-sivy",
+            },
+        ],
+        keys: ["intern"],
+    },
+    {
+        name: "Naisheng Ye",
+        image: "/assets/person/naisheng_ye.jpg",
+        desc: "Zhejiang University",
+        image_s: "/assets/person/naisheng_ye_s.jpg",
+        // desc: "复旦大学（25级联培）",
+        links: [
+            {
+                icon: "email",
+                url: "mailto:yns.victory@gmail.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/Y-vic",
+            },
+        ],
+        keys: ["intern"],
+    },
+    {
+        name: "Shijia Peng",
+        image: "/assets/person/shijia_peng.jpg",
+        desc: "Shenzhen University",
+        image_s: "/assets/person/shijia_peng_s.jpg",
+        links: [
+            {
+                icon: "homepage",
+                url: "https://shijiapeng03.github.io/",
+            },
+            {
+                icon: "email",
+                url: "mailto:jiagedainifei@gmail.com",
+            },
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?hl=en&user=T3lq19kAAAAJ",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/ShijiaPeng03",
+            },
+            {
+                icon: "xiaohongshu",
+                url: "https://www.xiaohongshu.com/user/profile/5d2ff1090000000012035ac6",
+            },
+        ],
+        keys: ["intern"],
+    },
+    {
+        name: "Zhuoheng Li",
+        image: "/assets/person/zhuoheng_li.jpg",
+        desc: "The University of Hong Kong (HKU)",
+        image_s: "/assets/person/zhuoheng_li_s.jpg",
+        links: [
+            {
+                icon: "email",
+                url: "mailto:lizhuoheng@opendrivelab.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/StarCycle/StarCycle",
+            },
+            {
+                icon: "linkedin",
+                url: "https://www.linkedin.com/in/lizhuoheng/",
+            },
+            {
+                icon: "huggingface",
+                url: "https://huggingface.co/StarCycle",
+            },
+        ],
+        keys: ["intern"],
+    },
+    {
+        name: "Haochen Liu",
+        image: "/assets/person/haochen_liu.jpg",
+        desc: "Nanyang Technological University (NTU)",
+        image_s: "/assets/person/haochen_liu_s.jpg",
+        links: [
+            {
+                icon: "homepage",
+                url: "https://georgeliu233.github.io/",
+            },
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=iizqKUsAAAAJ&hl",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/georgeliu233",
+            },
+        ],
+        keys: ["intern"],
+    },
+    {
+        name: "Shadi Hamdan",
+        image: "/assets/person/shadi_hamdan.jpg",
+        desc: "Koç University",
+        image_s: "/assets/person/shadi_hamdan_s.jpg",
+        links: [
+            {
+                icon: "homepage",
+                url: "https://shadihamdan.com/",
+            },
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=I6bAD78AAAAJ",
+            },
+            {
+                icon: "email",
+                url: "mailto:shamdan17@ku.edu.tr",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/Shamdan17",
+            },
+        ],
+        keys: ["intern"],
+    },
+];
