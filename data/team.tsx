2d1f3ab17b53aa49e49f869d57fdb5d43a37994b
faikit <huijie.wang@hotmail.com>
2025-06-19 15:34:05 +0800

    .

--- data/team.tsx
+++ data/team.tsx
@@ -157,6 +157,52 @@ export const members: {
         keys: ["phd"],
     },
     {
+        name: "Yixuan Pan",
+        image: "/assets/person/yixuan_pan.jpg",
+        desc: "HKU (The University of Hong Kong)",
+        image_s: "/assets/person/yixuan_pan_s.jpg",
+        links: [
+            {
+                icon: "homepage",
+                url: "https://lzpyx.github.io/",
+            },
+            {
+                icon: "email",
+                url: "mailto:yixuanpan2024@gmail.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/lzpyx",
+            },
+        ],
+        keys: ["phd"],
+    },
+    {
+        name: "Zhuoheng Li",
+        image: "/assets/person/zhuoheng_li.jpg",
+        desc: "HKU (The University of Hong Kong)",
+        image_s: "/assets/person/zhuoheng_li_s.jpg",
+        links: [
+            {
+                icon: "email",
+                url: "mailto:lizhuoheng@opendrivelab.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/StarCycle/StarCycle",
+            },
+            {
+                icon: "linkedin",
+                url: "https://www.linkedin.com/in/lizhuoheng/",
+            },
+            {
+                icon: "huggingface",
+                url: "https://huggingface.co/StarCycle",
+            },
+        ],
+        keys: ["phd"],
+    },
+    {
         name: "Hanxue Zhang",
         image: "/assets/person/hanxue_zhang.jpg",
         image_s: "/assets/person/hanxue_zhang_s.jpg",
@@ -173,48 +219,6 @@ export const members: {
             {
                 icon: "github",
                 url: "https://github.com/jjxjiaxue",
-            },
-        ],
-        keys: ["phd"],
-    },
-    {
-        name: "Chengen Xie",
-        image: "/assets/person/chengen_xie.jpg",
-        image_s: "/assets/person/chengen_xie_s.jpg",
-        desc: "SJTU (Shanghai Jiao Tong University)",
-        links: [
-            {
-                icon: "scholar",
-                url: "https://scholar.google.com/citations?user=-Sk1x_gAAAAJ",
-            },
-            {
-                icon: "email",
-                url: "mailto:xiechengen@opendrivelab.com",
-            },
-            {
-                icon: "github",
-                url: "https://github.com/ChengenXie",
-            },
-        ],
-        keys: ["phd"],
-    },
-    {
-        name: "Haoran Jiang",
-        image: "/assets/person/haoran_jiang.jpg",
-        image_s: "/assets/person/haoran_jiang_s.jpg",
-        desc: "Fudan University",
-        links: [
-            {
-                icon: "scholar",
-                url: "https://scholar.google.com/citations?user=KYzm-k0AAAAJ",
-            },
-            {
-                icon: "email",
-                url: "mailto:jianghaoran2024@gmail.com",
-            },
-            {
-                icon: "github",
-                url: "https://github.com/HRRiver7",
             },
         ],
         keys: ["phd"],
@@ -239,27 +243,6 @@ export const members: {
             },
         ],
         keys: ["intern"],
-    },
-    {
-        name: "Yixuan Pan",
-        image: "/assets/person/yixuan_pan.jpg",
-        desc: "HKU (The University of Hong Kong)",
-        image_s: "/assets/person/yixuan_pan_s.jpg",
-        links: [
-            {
-                icon: "homepage",
-                url: "https://lzpyx.github.io/",
-            },
-            {
-                icon: "email",
-                url: "mailto:yixuanpan2024@gmail.com",
-            },
-            {
-                icon: "github",
-                url: "https://github.com/lzpyx",
-            },
-        ],
-        keys: ["phd"],
     },
     {
         name: "Jiazhi Yang",
@@ -312,6 +295,23 @@ export const members: {
         keys: ["alumni"],
     },
     {
+        name: "Modi Shi",
+        image: "/assets/person/modi_shi.jpg",
+        desc: "BUAA (Beijing University of Aeronautics and Astronautics)",
+        image_s: "/assets/person/modi_shi_s.jpg",
+        links: [
+            {
+                icon: "email",
+                url: "mailto:modishi@buaa.edu.cn",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/ModiShi",
+            },
+        ],
+        keys: ["phd"],
+    },
+    {
         name: "Longyan Wu",
         image: "/assets/person/longyan_wu.jpg",
         desc: "Fudan University",
@@ -326,7 +326,7 @@ export const members: {
                 url: "https://github.com/wly-Lilie",
             },
         ],
-        keys: ["intern"],
+        keys: ["phd"],
     },
     {
         name: "Caojun Wang",
@@ -347,7 +347,7 @@ export const members: {
                 url: "https://github.com/WCJ-BERT",
             },
         ],
-        keys: ["intern"],
+        keys: ["phd"],
     },
     {
         name: "Hongchen Li",
@@ -364,7 +364,24 @@ export const members: {
                 url: "https://github.com/lhcstation",
             },
         ],
-        keys: ["intern"],
+        keys: ["phd"],
+    },
+    {
+        name: "Zhenhua Wu",
+        image: "/assets/person/zhenhua_wu.jpg",
+        desc: "Sun Yat-sen University",
+        image_s: "/assets/person/zhenhua_wu_s.jpg",
+        links: [
+            {
+                icon: "email",
+                url: "mailto:wuzhenhua1122@gmail.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/ArthurWuzh",
+            },
+        ],
+        keys: ["phd"],
     },
     {
         name: "Haochen Tian",
@@ -388,21 +405,25 @@ export const members: {
         keys: ["intern"],
     },
     {
-        name: "Modi Shi",
-        image: "/assets/person/modi_shi.jpg",
-        desc: "BUAA (Beijing University of Aeronautics and Astronautics)",
-        image_s: "/assets/person/modi_shi_s.jpg",
+        name: "Haoran Jiang",
+        image: "/assets/person/haoran_jiang.jpg",
+        image_s: "/assets/person/haoran_jiang_s.jpg",
+        desc: "Fudan University",
         links: [
             {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=KYzm-k0AAAAJ",
+            },
+            {
                 icon: "email",
-                url: "mailto:modishi@buaa.edu.cn",
+                url: "mailto:jianghaoran2024@gmail.com",
             },
             {
                 icon: "github",
-                url: "https://github.com/ModiShi",
+                url: "https://github.com/HRRiver7",
             },
         ],
-        keys: ["intern"],
+        keys: ["phd"],
     },
     {
         name: "Jin Chen",
@@ -432,7 +453,28 @@ export const members: {
                 url: "https://www.xiaohongshu.com/user/profile/609cbe510000000001002922",
             },
         ],
-        keys: ["intern"],
+        keys: ["phd"],
+    },
+    {
+        name: "Chengen Xie",
+        image: "/assets/person/chengen_xie.jpg",
+        image_s: "/assets/person/chengen_xie_s.jpg",
+        desc: "SJTU (Shanghai Jiao Tong University)",
+        links: [
+            {
+                icon: "scholar",
+                url: "https://scholar.google.com/citations?user=-Sk1x_gAAAAJ",
+            },
+            {
+                icon: "email",
+                url: "mailto:xiechengen@opendrivelab.com",
+            },
+            {
+                icon: "github",
+                url: "https://github.com/ChengenXie",
+            },
+        ],
+        keys: ["phd"],
     },
     {
         name: "Jisong Cai",
@@ -496,23 +538,6 @@ export const members: {
         keys: ["intern"],
     },
     {
-        name: "Zhenhua Wu",
-        image: "/assets/person/zhenhua_wu.jpg",
-        desc: "Sun Yat-sen University",
-        image_s: "/assets/person/zhenhua_wu_s.jpg",
-        links: [
-            {
-                icon: "email",
-                url: "mailto:wuzhenhua1122@gmail.com",
-            },
-            {
-                icon: "github",
-                url: "https://github.com/ArthurWuzh",
-            },
-        ],
-        keys: ["intern"],
-    },
-    {
         name: "Shijia Peng",
         image: "/assets/person/shijia_peng.jpg",
         desc: "Shenzhen University",
@@ -540,31 +565,6 @@ export const members: {
             },
         ],
         keys: ["intern"],
-    },
-    {
-        name: "Zhuoheng Li",
-        image: "/assets/person/zhuoheng_li.jpg",
-        desc: "HKU (The University of Hong Kong)",
-        image_s: "/assets/person/zhuoheng_li_s.jpg",
-        links: [
-            {
-                icon: "email",
-                url: "mailto:lizhuoheng@opendrivelab.com",
-            },
-            {
-                icon: "github",
-                url: "https://github.com/StarCycle/StarCycle",
-            },
-            {
-                icon: "linkedin",
-                url: "https://www.linkedin.com/in/lizhuoheng/",
-            },
-            {
-                icon: "huggingface",
-                url: "https://huggingface.co/StarCycle",
-            },
-        ],
-        keys: ["phd"],
     },
     {
         name: "Haochen Liu",
