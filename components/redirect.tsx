7c706017a2df4881a35e61ba5e14ed718984c4ea
wly-Lilie <wu_long_yan@163.com>
2025-09-28 15:20:28 +0800

    freetacman

--- components/redirect.tsx
+++ components/redirect.tsx
@@ -89,7 +89,7 @@ export default function Redirect() {
         redirect('/RoboDual/')
     }
     if (
-        pathname.toLowerCase().startsWith('/freetacman') ||
+        pathname.toLowerCase().startsWith('/freetacman')
         // pathname.toLowerCase().startsWith('/blog/freetacman')
     ) {
         redirect('https://opendrivelab.com/FreeTacMan')
