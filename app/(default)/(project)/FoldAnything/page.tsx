b706c08ed3c2df6bba9096f7104241749e9589c3
faikit <huijie.wang@hotmail.com>
2025-12-15 21:04:37 +0800

    .

--- app/(default)/(project)/FoldAnything/page.tsx
+++ app/(default)/(project)/FoldAnything/page.tsx
@@ -62,8 +62,8 @@ export default function Home() {
 
 
                     <div className="w-full flex flex-col items-center">
-                        <h2 className="w-full max-w-7xl mt-20 text-t1 font-bold"> 
-                            <Link href="#service" className="scroll-mt-32 group flex items-center flex-row" id="service">
+                        <h2 className="w-full max-w-7xl mt-20 text-3xl font-bold"> 
+                            <Link href="#veni" className="scroll-mt-32 group flex items-center flex-row" id="veni">
                                 <h2>
                                     Veni: Mining Failure Modes from On-Policy Experience
                                 </h2>
@@ -78,14 +78,54 @@ export default function Home() {
                             </Link>
                         </h2>
                     </div>
-                    <div className="mt-20">
-
+                    <div className="mt-6 leading-relaxed">
+                        文字部分：Turning Stumbles into Strides: Mode Consistency between Data and model. (P_train (human action), Q_model (action projection, weights), P_test (policy action) 三者).  ( 0 - 20%, 基建)文字部分：Turning Stumbles into Strides: Mode Consistency between Data and model. (P_train (human action), Q_model (action projection, weights), P_test (policy action) 三者).  ( 0 - 20%, 基建)文字部分：Turning Stumbles into Strides: Mode Consistency between Data and model. (P_train (human action), Q_model (action projection, weights), P_test (policy action) 三者).  ( 0 - 20%, 基建)文字部分：Turning Stumbles into Strides: Mode Consistency between Data and model. (P_train (human action), Q_model (action projection, weights), P_test (policy action) 三者).  ( 0 - 20%, 基建)文字部分：Turning Stumbles into Strides: Mode Consistency between Data and model. (P_train (human action), Q_model (action projection, weights), P_test (policy action) 三者).  ( 0 - 20%, 基建)文字部分：Turning Stumbles into Strides: Mode Consistency between Data and model. (P_train (human action), Q_model (action projection, weights), P_test (policy action) 三者).  ( 0 - 20%, 基建)文字部分：Turning Stumbles into Strides: Mode Consistency between Data and model. (P_train (human action), Q_model (action projection, weights), P_test (policy action) 三者).  ( 0 - 20%, 基建)文字部分：Turning Stumbles into Strides: Mode Consistency between Data and model. (P_train (human action), Q_model (action projection, weights), P_test (policy action) 三者).  ( 0 - 20%, 基建)文字部分：Turning Stumbles into Strides: Mode Consistency between Data and model. (P_train (human action), Q_model (action projection, weights), P_test (policy action) 三者).  ( 0 - 20%, 基建)文字部分：Turning Stumbles into Strides: Mode Consistency between Data and model. (P_train (human action), Q_model (action projection, weights), P_test (policy action) 三者).  ( 0 - 20%, 基建)文字部分：Turning Stumbles into Strides: Mode Consistency between Data and model. (P_train (human action), Q_model (action projection, weights), P_test (policy action) 三者).  ( 0 - 20%, 基建)文字部分：Turning Stumbles into Strides: Mode Consistency between Data and model. (P_train (human action), Q_model (action projection, weights), P_test (policy action) 三者).  ( 0 - 20%, 基建)
                     </div>
 
-                    
+
 
                     <div className="w-full flex flex-col items-center">
-                        <Separator className="max-w-7xl mt-20"/>
+                        <h2 className="w-full max-w-7xl mt-20 text-3xl font-bold"> 
+                            <Link href="#vidi" className="scroll-mt-32 group flex items-center flex-row" id="vidi">
+                                <h2>
+                                    Vidi: Model Algebra as a Shortcut for Mode Extrapolation
+                                </h2>
+                                <div className="min-w-10">
+                                    <span className="ml-6 hidden group-hover:inline-block size-10 text-foreground">
+                                        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor">
+                                            <path d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212" strokeWidth="2.5" strokeLinecap="round"/>
+                                            <path d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373" strokeWidth="2.5" strokeLinecap="round"/>
+                                        </svg>
+                                    </span> 
+                                </div>
+                            </Link>
+                        </h2>
+                    </div>
+                    <div className="mt-6 leading-relaxed">
+                    文字部分：Model Arithmetic （20% 到80%, 性能）文字部分：Model Arithmetic （20% 到80%, 性能）文字部分：Model Arithmetic （20% 到80%, 性能）文字部分：Model Arithmetic （20% 到80%, 性能）文字部分：Model Arithmetic （20% 到80%, 性能）文字部分：Model Arithmetic （20% 到80%, 性能）文字部分：Model Arithmetic （20% 到80%, 性能）文字部分：Model Arithmetic （20% 到80%, 性能）
+                    </div>
+
+
+
+                    <div className="w-full flex flex-col items-center">
+                        <h2 className="w-full max-w-7xl mt-20 text-3xl font-bold"> 
+                            <Link href="#vici" className="scroll-mt-32 group flex items-center flex-row" id="vici">
+                                <h2>
+                                    Vici: Distribution-aware Advantage as Mode-Orchestrator
+                                </h2>
+                                <div className="min-w-10">
+                                    <span className="ml-6 hidden group-hover:inline-block size-10 text-foreground">
+                                        <svg viewBox="0 0 24 24" fill="none" stroke="currentColor">
+                                            <path d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212" strokeWidth="2.5" strokeLinecap="round"/>
+                                            <path d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373" strokeWidth="2.5" strokeLinecap="round"/>
+                                        </svg>
+                                    </span> 
+                                </div>
+                            </Link>
+                        </h2>
+                    </div>
+                    <div className="mt-6 leading-relaxed">
+                        文字部分：Stage Orchestrator (80%-100%, 拔尖)文字部分：Stage Orchestrator (80%-100%, 拔尖)文字部分：Stage Orchestrator (80%-100%, 拔尖)文字部分：Stage Orchestrator (80%-100%, 拔尖)文字部分：Stage Orchestrator (80%-100%, 拔尖)文字部分：Stage Orchestrator (80%-100%, 拔尖)文字部分：Stage Orchestrator (80%-100%, 拔尖)
                     </div>
 
 
